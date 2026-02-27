a305e81246745a2fe3d30cf98f33cab435ab1757
Salvador López Galván <salvadorlopezglvn2@gmail.com>
2026-02-26 18:28:19 -0800

    m

---
+++ OOP/Zombie.py
@@ -0,0 +1,11 @@
+from Enemigo import *
+
+class Zombie(Enemigo):
+    def _init_(self, puntos_energia=10, ataque=1):
+        super()._init_(tipo_enemigo='Zombie', puntos_energia=puntos_energia,ataque=ataque)
+
+    def habla(self):
+        print("Hummmm.....*")
+
+    def propagar_enfermedad(self):
+        print("El Zombie esta tratando de propagar la enfermedad!!")
