5b2ddc276f4fb25e2f53d21e5e7b623b1f247909
Salvador López Galván <salvadorlopezglvn2@gmail.com>
2026-02-26 18:48:14 -0800

    m

---
+++ OOP/Ogro.py
@@ -0,0 +1,8 @@
+from Enemigo import *
+
+class Ogro(Enemigo):
+    def _init_(self, puntos_energia=20, ataque=3):
+        super()._init_(tipo_enemigo='Ogro', puntos_energia=puntos_energia, ataque=ataque)
+
+    def habla(self):
+        print("Ogro aplasta todo!!!")
